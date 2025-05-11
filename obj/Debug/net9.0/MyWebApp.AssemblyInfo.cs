85f880ef43ec613d900ddab3fb951a3ae3b3301a
Sarthak Neupane <sarthakneup11@gmail.com>
2025-05-11 22:02:13 +0545

    update code

--- obj/Debug/net9.0/MyWebApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deabaf002585bd1dc94d609a637a23b6b9c1d6f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da361cad095c695031506df2aec030f367ad5cd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
