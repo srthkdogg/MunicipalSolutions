29a5ee229a4a6c9acf06b7d9310941bf35857707
Sarthak Neupane <sarthakneup11@gmail.com>
2025-05-11 19:14:09 +0545

    update code

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -31,4 +31,18 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+    public IActionResult Announcements()
+    {
+        return View();
+    }
+
+    public IActionResult Discussions()
+    {
+        return View();
+    }
+
+    public IActionResult Services()
+    {
+        return View();
+    }
 }
--- obj/Debug/net9.0/MyWebApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a5d1533f5cb566514bacedc292a990cf422d397")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58bf0cc2710b2b3071c75176c92edac57a1afb38")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
