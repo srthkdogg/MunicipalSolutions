5ba0c5a031e2b9c910fabf5b78859d3838f97c20
Sarthak Neupane <sarthakneup11@gmail.com>
2025-05-12 09:54:56 +0545

    update code

--- Models/Announcement.cs
+++ Models/Announcement.cs
@@ -2,6 +2,7 @@ namespace MyWebApp.Models
 {
     public class Announcement
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Message { get; set; }
     }
